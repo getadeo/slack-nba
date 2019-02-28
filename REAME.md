6f159dd7461cb25b53db673d2ddab5c85d9f2db2
Ge Tadeo <mr.genesis.tadeo@gmail.com>
2019-02-28 14:09:25 +0800

    Add today game details fetcher

--- REAME.md
+++ REAME.md
@@ -1,1 +1,13 @@
 #NBA Slack Slash Command App
+
+##Scoreboard
+
+##Team Stats
+
+##League Stats Leader
+
+##MVP Candidates
+
+##DOTY Candidates
+
+##Standings
--- server.js
+++ server.js
@@ -8,34 +8,54 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let nbaAPIURL = "http://data.nba.net/10s/prod/v1/"
+let nbaAPIURL = "http://data.nba.net/10s"
 
 app.get('/', function (req, res) {
   res.send('hello world');
 });
+
 
 app.post('/v1/nba', function (req, res) {
   console.log(req.body);
   let today = new Date();
   today = moment(today).subtract(16, 'hours').format('YYYYMMDD')
   if (req.body.text === 'scoreboard') {
-    console.log('Scoreboard');
-    let scoreBoardEndpoint = nbaAPIURL + today + "/scoreboard.json";
-    request(scoreBoardEndpoint, function(error, response, body) {
-      console.log('request endpoint: ', scoreBoardEndpoint)
+
+    let todayEndpoint = nbaAPIURL + "/prod/v1/today.json";
+    let todayObject = {};
+    let scoreBoardEndpoint = "";
+    request(todayEndpoint, function(error, response, body) {
+      console.log('Fetching todays game details');
+      console.log('request endpoint: ', todayEndpoint);
       console.log('error: ', error);
       console.log('statusCode: ', response && response.statusCode);
-      let scoreBoard = ""
-      if (response.statusCode === 200) {
-        let gamesObject = JSON.parse(body);
-        for (let game of gamesObject.games ){
-          scoreBoard += `${game.vTeam.score} ${game.vTeam.triCode} (${game.vTeam.win}-${game.vTeam.win}) VS ${game.hTeam.score} ${game.hTeam.triCode} (${game.hTeam.win}-${game.hTeam.win})\n`;
-        }
-        console.log('scoreboard: \n', scoreBoard);
-        res.statusCode = 200;
-        return res.send(scoreBoard);
+
+      if (response.statusCode === 200){
+        todayObject = JSON.parse(body);
+        scoreBoardEndpoint = nbaAPIURL + todayObject.links.currentScoreboard;
+        request(scoreBoardEndpoint, function (error, response, body) {
+          console.log('request endpoint: ', scoreBoardEndpoint)
+          console.log('error: ', error);
+          console.log('statusCode: ', response && response.statusCode);
+
+          let scoreBoard = ""
+          if (response.statusCode === 200) {
+            let gamesObject = JSON.parse(body);
+            for (let game of gamesObject.games) {
+              scoreBoard += `${game.vTeam.score} ${game.vTeam.triCode} (${game.vTeam.win}-${game.vTeam.win}) VS ${game.hTeam.score} ${game.hTeam.triCode} (${game.hTeam.win}-${game.hTeam.win})\n`;
+            }
+            console.log('scoreboard: \n', scoreBoard);
+            res.statusCode = 200;
+            return res.send(scoreBoard);
+          }
+        })
       }
     })
+
+    console.log('Scoreboard');
+    console.log(scoreBoardEndpoint);
+    // let scoreBoardEndpoint = nbaAPIURL + today + "/scoreboard.json";
+
   }
 });
 
